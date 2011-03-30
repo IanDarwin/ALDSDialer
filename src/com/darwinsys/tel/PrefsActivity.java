5ebe83d9b3778aeb15888812cb6010160e6a44d0
IanDarwin <>
2011-03-30 01:14:40 +0000

    Need a Main Activity, PrefsActivity, to enable/disable things.

---
+++ src/com/darwinsys/tel/PrefsActivity.java
@@ -0,0 +1,15 @@
+package com.darwinsys.tel;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PrefsActivity extends Activity {
+
+		@Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.main);
+		}
+}
